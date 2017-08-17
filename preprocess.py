4300d3bca6247dfe0c3c56c8e59f245506381061
Yusuke Yasuda <yusuke.007.yasud@gmail.com>
2017-08-17 11:48:23 +0900

    add swims dataset

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import argparse
 import os
 from multiprocessing import cpu_count
 from tqdm import tqdm
-from datasets import blizzard, ljspeech
+from datasets import blizzard, ljspeech, siwis
 from hparams import hparams
 
 
@@ -22,6 +22,14 @@ def preprocess_ljspeech(args):
   write_metadata(metadata, out_dir)
 
 
+def preprocess_siwis(args):
+  in_dir = os.path.join(args.base_dir, 'The_SIWIS_French_Speech_Synthesis_Database/SiwisFrenchSpeechSynthesisDatabase/')
+  out_dir = os.path.join(args.base_dir, args.output)
+  os.makedirs(out_dir, exist_ok=True)
+  metadata = siwis.build_from_path(in_dir, out_dir, args.num_workers, tqdm=tqdm)
+  write_metadata(metadata, out_dir)
+
+
 def write_metadata(metadata, out_dir):
   with open(os.path.join(out_dir, 'train.txt'), 'w', encoding="utf-8") as f:
     for m in metadata:
@@ -35,15 +43,17 @@ def write_metadata(metadata, out_dir):
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--base_dir', default=os.path.expanduser('~/tacotron'))
+  parser.add_argument('--base_dir', default=os.path.expanduser('.'))
   parser.add_argument('--output', default='training')
-  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech'])
+  parser.add_argument('--dataset', required=True, choices=['blizzard', 'ljspeech', 'siwis'])
   parser.add_argument('--num_workers', type=int, default=cpu_count())
   args = parser.parse_args()
   if args.dataset == 'blizzard':
     preprocess_blizzard(args)
   elif args.dataset == 'ljspeech':
     preprocess_ljspeech(args)
+  elif args.dataset == 'siwis':
+    preprocess_siwis(args)
 
 
 if __name__ == "__main__":
---
+++ util/textinput_fr.py
@@ -0,0 +1,62 @@
+import re
+import unicodedata
+from util import numbers
+
+
+# Input alphabet (63 symbols), plus french (26 symbols):
+_pad         = '_'
+_eos         = '~'
+_uppercase   = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+_lowercase   = 'abcdefghijklmnopqrstuvwxyz'
+_punctuation = '!\'(),-.:;?'
+_space       = ' '
+_fr_uppercase = 'ÀÂÇÈÉÊËÎÏÔÙÛÜ'
+_fr_lowercase = 'àâçèéêëîïôùûü'
+
+_valid_input_chars = _uppercase + _lowercase + _punctuation + _space + _fr_uppercase + _fr_lowercase
+_trans_table = str.maketrans({chr(i): ' ' for i in range(256) if chr(i) not in _valid_input_chars})
+
+_normal_symbols = _pad + _eos + _valid_input_chars
+_num_normal_symbols = len(_normal_symbols)
+_char_to_id = {c: i for i, c in enumerate(_normal_symbols)}
+_id_to_char = {i: c for i, c in enumerate(_normal_symbols)}
+_num_symbols = _num_normal_symbols
+_whitespace_re = re.compile(r'\s+')
+
+
+def num_symbols():
+  '''Returns number of symbols in the alphabet.'''
+  return _num_symbols
+
+
+def to_sequence(text, force_lowercase=True, expand_abbreviations=True):
+  '''Converts a string of text to a sequence of IDs for the symbols in the text'''
+  text = text.strip()
+  text = text.replace('"', '')
+  text = unicodedata.normalize('NFKD', text).encode('utf-8', 'ignore').decode()
+
+  sequence = []
+  sequence += _text_to_sequence(text, force_lowercase, expand_abbreviations)
+  sequence.append(_char_to_id[_eos])
+  return sequence
+
+
+def to_string(sequence, remove_eos=False):
+  '''Returns the string for a sequence of characters.'''
+  s = ''
+  for sym in sequence:
+    if sym < _num_normal_symbols:
+      s += _id_to_char[sym]
+  s = s.replace('}{', ' ')
+  if remove_eos and s[-1] == _eos:
+    s = s[:-1]
+  return s
+
+
+def _text_to_sequence(text, force_lowercase, expand_abbreviations):
+  text = text.translate(_trans_table)
+  if force_lowercase:
+    text = text.lower()
+  text = re.sub(_whitespace_re, ' ', text)
+  return [_char_to_id[c] for c in text]
+
